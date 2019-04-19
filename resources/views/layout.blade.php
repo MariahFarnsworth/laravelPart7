d7ea03d4daa916356ea0bccd95125bebe4570150
Mariah <skittles333dawn@gmail.com>
2019-04-19 19:38:39 -0400

    lesson 17: form acttion considerations

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css"/>
+    <style>
+        .is-complete{
+            text-decoration: line-through;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
